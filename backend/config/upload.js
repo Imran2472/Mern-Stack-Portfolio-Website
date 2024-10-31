c21779d02e0c4901d822548896eb23a60cb3e4df
Jibranaa <162036965+Imran2472@users.noreply.github.com>
2024-10-31 15:49:26 +0500

    Update gfiles

--- backend/config/upload.js
+++ backend/config/upload.js
@@ -1,8 +1,5 @@
 import multer from "multer";
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "uploads"));
-  },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "__" + file.originalname);
   },
